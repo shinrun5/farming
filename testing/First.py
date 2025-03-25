c7569c472c87adf234aeff1657255040d888018e
shinrun5 <321danielhe@gmail.com>
2025-03-25 10:35:23 -0400

    ok so I need to learn how to remove tags from text

--- testing/First.py
+++ testing/First.py
@@ -3,4 +3,8 @@ import requests
 url = "https://osu.ppy.sh/community/forums/topics/2047212?n=1cl"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
-print(soup)
+"""print(soup.get_text()) //get all text
+print(soup.find(class_="bbcode"))"""
+
+results = soup.find_all(class_="bbcode")
+print(results)
