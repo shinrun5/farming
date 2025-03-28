688f818f9c64a9bd681a52fdfceb75f8702e03db
shinrun5 <321danielhe@gmail.com>
2025-03-27 21:50:01 -0400

    need to work on removing tags from text

---
+++ testing/remove.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+url = "https://osu.ppy.sh/community/forums/topics/2047212?n=1cl"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+"""print(soup.get_text()) //get all text
+print(soup.find(class_="bbcode"))"""
+
+results = soup.find_all('div', class_="bbcode")
+
+print(results)
