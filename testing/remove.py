bdb87ef0ee7011fb9410cc202d71a2f1e3837d65
shinrun5 <321danielhe@gmail.com>
2025-03-31 22:03:57 -0400

    dawg im wasting so much time

--- testing/remove.py
+++ testing/remove.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+#inlcude <string>
+
 url = "https://osu.ppy.sh/community/forums/topics/2047212?n=1cl"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -7,5 +9,7 @@ soup = BeautifulSoup(page.text, 'html.parser')
 print(soup.find(class_="bbcode"))"""
 
 results = soup.find_all('div', class_="bbcode")
+
+
 
 print(results)
