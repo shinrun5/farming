8f11f61e024389fa0791c438e67478958a5e27af
shinrun5 <321danielhe@gmail.com>
2025-04-01 11:43:30 -0400

    I finally got to search for last user name and id

--- testing/remove.py
+++ testing/remove.py
@@ -8,8 +8,16 @@ soup = BeautifulSoup(page.text, 'html.parser')
 """print(soup.get_text()) //get all text
 print(soup.find(class_="bbcode"))"""
 
-results = soup.find_all('div', class_="bbcode")
+info = soup.find_all('a', class_="forum-post-info__row forum-post-info__row--username js-usercard")[-1]
+print(info.get_text())
+print(info.get('data-user-id'))
+
+
+#results = soup.find_all('div', class_="bbcode")[-1]
+#for row in results:
+#    print(row)
 
 
 
-print(results)
+
+#print(results)
