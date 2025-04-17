59ebefc0b58cdc01e84e7cefc8a7bc1f2e172ddf
shinrun5 <321danielhe@gmail.com>
2025-04-16 21:08:17 -0400

    Added post checker to testbot

--- oss/testbot.py
+++ oss/testbot.py
@@ -14,7 +14,7 @@ def check_post(link):
     page = requests.get(link)
     soup = BeautifulSoup(page.text, 'html.parser')
     info = soup.find_all('a', class_="forum-post-info__row forum-post-info__row--username js-usercard")[-1]
-    print(soup.find_all('div', class_="bbcode")[-1])
+    print(soup.find_all('div', class_="forum-post__content--main")[-1])
     print(info.get_text())
     print(info.get('data-user-id'))
 
